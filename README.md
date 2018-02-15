67ab41f62afae9c974efce195de8beaac053c1c8
Kurset <haoyuace@163.com>
2018-02-15 23:02:24 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# 项目说明
+
+来源于[编程风格](https://book.douban.com/subject/27102599/)，介绍了各种编程风格代码，但书中代码是用python写的，所以自己又用node重写了一遍。
+
+原书python代码库 https://github.com/crista/exercises-in-programming-style
---
+++ monolith-style.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+let wordFregs = [];
+let stopWords = [];
+
+const stopWordsData = fs.readFileSync('./stop_words.txt', 'utf8');
+stopWords = stopWordsData.split(',');
+
+let bookData = fs.readFileSync(process.argv[2] ? process.argv[2] : './pride-and-prejudice.txt', 'utf8');
+
+let startWordIndex = undefined;
+for (let i = 0,len = bookData.length; i < len; i += 1) {
+    let s = bookData[i];
+    if (startWordIndex === undefined) {
+        if (/[0-9a-zA-Z]/g.test(s)) {
+            startWordIndex = i;
+        }
+    } else {
+        if (!/[0-9a-zA-Z]/g.test(s)) {
+            word = bookData.slice(startWordIndex, i);
+            word = word.toLowerCase();
+            startWordIndex = undefined;
+            if (stopWords.indexOf(word) === -1 && word.length > 1) {
+                // 如果不在助词中
+                let found = false;
+                let pairIndex = 0;
+                for (let j = 0, fLen = wordFregs.length;j < fLen; j += 1) {
+                    if (word === wordFregs[j][0]) {
+                        found = true;
+                        wordFregs[j][1] += 1;
+                        break;
+                    }
+                }
+                if (found === false) {
+                    wordFregs.push([word, 1]);
+                }
+            }
+        }
+    }
+}
+
+wordFregs.sort(function(a, b) {
+    return b[1] - a[1];
+});
+
+for (let i = 0, len = 20; i < len; i += 1) {
+    console.log(wordFregs[i][0], '——', wordFregs[i][1]);
+};
